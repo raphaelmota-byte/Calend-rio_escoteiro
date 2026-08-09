8dbaeb359ec5c437beb7992b46f5162d3d67f362
raphaelmota-byte <raphamota08@gmail.com>
2026-08-09 14:34:12 -0300

    adicionado a função de filtro no EventoViewSet

--- backend/eventos/views.py
+++ backend/eventos/views.py
@@ -13,9 +13,24 @@ class EventoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
     
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        categoria_id = self.request.query_params.get("categoria")
+        secao_id = self.request.query_params.get("secao")
+
+        if categoria_id:
+            queryset = queryset.filter(categoria_id=categoria_id)
+
+        if secao_id:
+            queryset = queryset.filter(secoes=secao_id)
+
+        return queryset
+    
 class SecaoViewSet(viewsets.ModelViewSet):
     queryset = Secao.objects.all()
     serializer_class = SecaoSerializer
+    
+    
     
 class ParticipanteViewSet(viewsets.ModelViewSet):
     queryset = Participante.objects.all()
